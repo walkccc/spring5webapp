864a5fa1df29c3a27662971111f94bb3aad8baa1
Jay Chen <walkccray@gmail.com>
2020-06-11 01:37:22 +0800

    Lint

--- src/test/java/walkccc/springframework/spring5webapp/Spring5webappApplicationTests.java
+++ src/test/java/walkccc/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -5,9 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Spring5webappApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
