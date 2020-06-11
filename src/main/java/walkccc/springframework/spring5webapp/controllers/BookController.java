d69ab236fcef345f8c9db532f5a1ffd2c93b4280
Jay Chen <walkccray@gmail.com>
2020-06-12 06:22:20 +0800

    Add BookController

---
+++ src/main/java/walkccc/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,21 @@
+package walkccc.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import walkccc.springframework.spring5webapp.repositories.BookRepository;
+
+@Controller
+public class BookController {
+  private final BookRepository bookRepository;
+
+  public BookController(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  @RequestMapping("/books")
+  public String getBooks(Model model) {
+    model.addAttribute("books", bookRepository.findAll());
+    return "books";
+  }
+}
